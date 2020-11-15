b43705b9011e76986c8df45f83f94f5d1f179ad8
THOBY555 <71563419+THOBY555@users.noreply.github.com>
2020-11-15 10:07:54 +0300

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Randommessage;
+
+use PocketMine/server;
+use PocketMine/player;
+
+use PocketMine/plugin/pluginbase;
+
+use PocketMine/command/command;
+use PocketMine/command/commandsender;
+
+use PocketMine/event/listevent
+
+class main extends pluginbase implements listener {
+
+	public function onEnable{}{
+		$this->getlogge>{}->info{"its Enable He He Boy"}
+	}
+
+	public function onDisabel{}{
+		$this->getlogger{}info{"its disbled B)"}
+
+		public function addcslashes(str, charlist)
+	}
+}
+auto msg_send(666, msg, Hi player its bot (～￣▽￣)～)
+ auto msg_send(132, msg, lts go)
+ auto msg_send(434, msg, Welcome to this server he he)
+ auto msg_send(777, msg,Welcome to thi server he he B) )
